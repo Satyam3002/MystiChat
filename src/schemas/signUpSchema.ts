039df9dac79e0e76891547f9215d9e76c0bb78b8
Satyam3002 <satyamlaheri3@gmail.com>
2025-02-14 12:44:54 +0530

    created Schema

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod';
+
+export const usernameValidation =  z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long" })
+    .max(20, { message: "Username cannot be longer than 20 characters" })
+    .regex(/^[a-zA-Z0-9_]*$/, { message: "Username can only contain alphanumeric characters and underscores" });
+
+    export const signUpSchema = z.object({
+        username: usernameValidation,
+        email: z.string().email({ message: "Please use a valid email address" }),
+        password: z.string().min(8, { message: "Password must be at least 8 characters long" }),
+
+    });
