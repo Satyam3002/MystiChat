1fd0c5de1d964fad4355def1e3b10be17f100921
Satyam3002 <satyamlaheri3@gmail.com>
2025-02-24 19:00:44 +0530

    created api for signup and add helper function for sending email

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -4,7 +4,8 @@ export const usernameValidation =  z
     .string()
     .min(3, { message: "Username must be at least 3 characters long" })
     .max(20, { message: "Username cannot be longer than 20 characters" })
-    .regex(/^[a-zA-Z0-9_]*$/, { message: "Username can only contain alphanumeric characters and underscores" });
+    .regex(/^[a-zA-Z0-9_]*$/, { message: "Username can only contain alphanumeric characters and underscores" 
+    });
 
     export const signUpSchema = z.object({
         username: usernameValidation,
